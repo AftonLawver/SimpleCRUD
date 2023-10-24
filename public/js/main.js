91858cc69e13e49b5f44fbdb8f24e2f339b814b5
Afton Lawver <lawverap25@gmail.com>
2023-10-24 16:39:11 -0500

    Implemented create and read functionality from the database.

--- public/js/main.js
+++ public/js/main.js
@@ -2,24 +2,50 @@ const createNewEmployeeButton = document.getElementById("createEmployeeButton");
 createNewEmployeeButton.addEventListener('click', () => {
     document.getElementById("form-container").style.display = "block";
     createNewEmployeeButton.style.display = "none";
-    document.getElementById("submitEmployeeButton").style.display = "block";
+    document.getElementById("submitEmployeeButton").style.display = "inline";
 })
 
-function getEmployees() {
+const submitEmployeeButton = document.getElementById("submitEmployeeButton");
+submitEmployeeButton.addEventListener('click', () => {
+    const data = extractUserInformation();
     const options = {
-        method: 'GET',
+        method: 'POST',
         headers: {
             'Content-Type': "application/json"
         },
+        body: JSON.stringify(data)
     };
-    fetch('/', options)
+    fetch('/create', options)
         .then((response) => response.json())
         .then((data) => {
             console.log(data);
-
         })
+})
+
+const extractUserInformation = function() {
+    let name = document.getElementById('nameInput').value;
+    let occupation = document.getElementById('occupationInput').value;
+    let salary = document.getElementById('salaryInput').value;
+    return {name: name, occupation: occupation, salary: salary};
 }
 
-getEmployees();
+// function getEmployees() {
+//     const options = {
+//         method: 'GET',
+//         headers: {
+//             'Content-Type': "application/json"
+//         },
+//     };
+//     fetch('/employees', options)
+//         .then((response) => response.json())
+//         .then((data) => {
+//             console.log(data);
+//
+//
+//         })
+//         .catch((error) => {
+//             console.error("Error: ", error);
+//         });
+// }
 
 
--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
-const path = require("path");
 const app = express();
+const path = require("path");
 require('dotenv').config();
 const PORT = process.env.PORT;
-app.use(express.static(path.join(__dirname, 'public')));
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.json());
+app.set("view engine", "ejs");
+
 const mysql = require('mysql2');
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/index.html'));
-});
-
+// app.set('views', path.join(__dirname, 'views'));
 
 const connection = mysql.createConnection({
     host: process.env.MYSQL_HOST,
@@ -18,19 +21,48 @@ const connection = mysql.createConnection({
     database: process.env.MYSQL_DATABASE
 });
 
-//creating route for the app to get all employees
-app.get('/', (req, res) => {
+app.get('/', function(req, res) {
     connection.connect();
-    connection.query('SELECT * from employees', function(err, rows, fields) {
+    const q = "SELECT * from employees"
+    connection.query(q, function(err, data, fields) {
         if (!err) {
-            console.log(JSON.stringify(rows));
-            res.send(JSON.stringify(rows));
+            const users = data;
+            res.render("index.ejs", { users });
+            // return res.json(data);
+        } else {
+            console.log('Error while performing Query.');
+        }
+    });
+});
+
+
+
+// creating route for the app to get all employees
+app.get('/employees', (req, res) => {
+    connection.connect();
+    const q = "SELECT * from employees"
+    connection.query(q, function(err, data, fields) {
+        if (!err) {
+            return res.json(data);
         } else {
             console.log('Error while performing Query.');
         }
     });
     connection.end();
 });
+
+app.post("/create", (req, res) => {
+    const q = "INSERT INTO employees (`name`,`occupation`,`salary`) VALUES (?)"
+    const values = [
+        req.body.name,
+        req.body.occupation,
+        req.body.salary
+    ];
+    connection.query(q, [values], (err, data) => {
+        if (err) return res.json(err);
+        return res.json("Employee has been added successfully.");
+    })
+})
 
 app.listen(PORT, err => {
     if (err) {
